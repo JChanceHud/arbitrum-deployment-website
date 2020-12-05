d04e00ff45424d639e31235b7d8d8e5329c7b681
Chance Hudson <jchancehud@gmail.com>
2020-12-05 13:18:07 -0600

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -8,4 +8,4 @@ export const ROLLUP_FACTORIES: { [chainId: number]: string | undefined } = {
   4: '0x78D2d992bcfBCd0905603284C83248E5B14378e2', // rinkeby
   42: '0xee1250962014364aCf506061E66e78e65b8bCEEC' // kovan
 }
-export const ARBOS_HASH = '0xa3121bbe88d1aa990bef989652a34411dcb8b9428764315801ded4d1ab6a7188'
+export const ARBOS_HASH = '0x87ec7e22f108e912ad996ce8d5ae087e9e94e449d9a01ffcc3ca59384b5c3b82'

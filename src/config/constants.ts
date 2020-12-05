85b8fea52bca6b2689372f3b66fbe547cbb75a90
Chance Hudson <jchancehud@gmail.com>
2020-12-05 13:09:20 -0600

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -5,7 +5,7 @@ export const DEV_DOC_URL =
 // seems like metamask switched rinkeby and kovan chain ids?
 export const ROLLUP_FACTORIES: { [chainId: number]: string | undefined } = {
   // 3: '0xb391A11CAeE4FACD5d99335b3bc9daD237cb855e', // ropsten
-  // 4: '0x356e19929FCb4973c131d558300E3E353cb8e1C9', // rinkeby
+  4: '0x78D2d992bcfBCd0905603284C83248E5B14378e2', // rinkeby
   42: '0xee1250962014364aCf506061E66e78e65b8bCEEC' // kovan
 }
-export const ARBOS_HASH = '0x5bec2cc2daea1334022bfb3ec9da5912aafc7adf06ab29a3ba583ad6565d2c8e'
+export const ARBOS_HASH = '0xa3121bbe88d1aa990bef989652a34411dcb8b9428764315801ded4d1ab6a7188'
